92f8fae8d82e9af646bbb858d4010deb4e29d76a
Rangel <marcelo.rcs@icloud.com>
2022-10-03 20:51:45 -0300

    Fix message listener after discord.js update

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-const Discord = require('discord.js');
+const {Client, GatewayIntentBits, ActivityType, Collection, Message} = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent
+    ]
+ });
 
 const mudaeId = '432610292342587392';
 
 client.once('ready', () => {
     console.log('Amadeus is online!')
-    client.user.setActivity("reações na Mudae", {
-    type: Discord.ActivityType.Watching,
-  });
+    client.user.setActivity('❤️ na Mudae', { type: ActivityType.Playing });
 })
 
-client.on('message', message => {
-    if (message.author.id == mudaeId) {
+client.on('messageCreate', (message) =>{
+    if(message.author.id == mudaeId){
         message.react('❤️')
     }
 })
-
-
 
 
 
